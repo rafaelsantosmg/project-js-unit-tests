676f2a01834560d78b4ead238819a27279242cbc
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-09-29 19:05:17 -0300

    segundo requisito

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -15,11 +15,23 @@ const numbers = require('../src/numbers');
 */
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
-  it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
-    fail('Teste vazio!');
-    // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    // Escreva um teste em que a função recebe [' '] e retorna false
+  // it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
+  //   fail('Teste vazio!')
+  // });
+  // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+  it('Verifica se a função `numbers` retona `true` quando a função receber [1, 2, 3, 4, 5]', () => {
+    expect(numbers([1, 2, 3, 4, 5])).toEqual(true);
+  });
+  // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+  it('Verifica se função `numbers` retorna `false` quando a função receber [1, 2, "3", 4, 5]', () => {
+    expect(numbers([1, 2, '3', 4, 5])).toEqual(false);
+  });
+  // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+  it('Verifica se função `numbers` retorna `false` quando a função receber [1, "a", 3]', () => {
+    expect(numbers([1, 'a', 3])).toEqual(false);
+  });
+  // Escreva um teste em que a função recebe [' '] e retorna false
+  it('Verifica se função `numbers` retorna `false` quando a função receber [" "]', () => {
+    expect(numbers([' '])).toEqual(false);
   });
 });
